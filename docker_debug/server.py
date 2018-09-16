7287e990193458f1fc0519eb9ef6eeb9c91995af
Will Thames <will@thames.id.au>
2018-09-16 21:07:45 +1000

    Improve docker_debug cache busting
    Ensure / points to different stylesheets rather than updating
    the existing stylesheet, which is liable to caching

--- docker_debug/server.py
+++ docker_debug/server.py
@@ -1,10 +1,15 @@
+from colour import colour
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 @app.route("/")
 def docker_debug():
     with open(os.environ.get("WWW_DATA", "helloworld.txt")) as f:
         data = f.read()
-    return render_template('docker_debug.j2', data=data, environs=os.environ)
+    response = make_response(render_template('docker_debug.j2', colour=colour, data=data, environs=os.environ))
+    response.headers['Cache-Control'] = 'max-age=0'
+    return response
